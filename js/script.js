9cf2ff883ff83fdb9548cd9c15a75e343ffd47fe
spencp99 <71106153+spencp99@users.noreply.github.com>
2020-10-13 18:33:56 -0400

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+/* Navbar */
+
+// get the hamburger element
+
+// get the menu element
+
+// get the overlay element
+
+// define toggle function
+
+// execute toggle function from hamburger on click
+
+// exuecute toggle function from overlay on click
+
+/* Navbar */
