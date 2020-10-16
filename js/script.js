04f96615120e45ad788538070f81779359429eda
spencp99 <spencp99@live.unc.edu>
2020-10-15 17:41:07 -0700

    final

--- js/script.js
+++ js/script.js
@@ -1,15 +1,25 @@
 /* Navbar */
 
 // get the hamburger element
+var hamburger = document.getElementById("hamburger");
 
 // get the menu element
+var menu = document.getElementById("menu");
 
 // get the overlay element
+var overlay = document.getElementById("overlay");
 
 // define toggle function
+function toggleMenu() {
+    menu.classList.toggle("active");
+    overlay.classList.toggle("active");
+}
 
 // execute toggle function from hamburger on click
+hamburger.addEventListener("click", toggleMenu);
 
 // exuecute toggle function from overlay on click
+menu.addEventListener("click", toggleMenu)
+overlay.addEventListener("click", toggleMenu);
 
 /* Navbar */
